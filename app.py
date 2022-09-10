23873687fe751f6960ed672f1ec861aef516356e
Frida A <dafrireece@gmail.com>
2022-09-10 13:51:27 +0000

    created links

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return render_template('home.html')
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/projects")
+def projects():
+  return render_template("projects.html")
+  
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port= 8080,debug=True)
+  app.run(host="0.0.0.0",debug=True)
